0fa8382e9a2ad91fcc4ec1813e76c46c6f5e0e7e
Patrycja Panasiuk <panasiuk.patrycja@gmail.com>
2020-11-13 11:12:22 +0100

    changes

--- app.js
+++ app.js
@@ -100,37 +100,30 @@ function showDivs(n) {
 }
 
 
-// REALISATION POP UP
+// // REALISATION POP UP
 
-// var modalReal = document.getElementsByClassName("realisation-modal");
-// var navReal = document.getElementsByClassName("navbar-area");
+// Get the modal
+var modal4 = document.getElementById("myModal--4");
 
-// // Get the button that opens the modal
-// var btnReal = document.getElementById("realisation-click");
+// Get the button that opens the modal
+var btn4 = document.getElementById("myBtn--4");
 
-// // Get the <span> element that closes the modal
-// var spanReal = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("close--4");
 
-// // When the user clicks the button, open the modal 
-// btnReal.onclick = function() {
-//   modalReal.style.display = "block";
-// }
+// When the user clicks on the button, open the modal
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
 
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-// window.onclick = function(event) {
-//   if (event.target == nav) {
-//     nav.style.display = "none !important";
-//   }
-// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
